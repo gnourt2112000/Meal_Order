4a88329798c40505f2b4852cd9c84c75a049075e
Truong <gnourt2112000@gmail.com>
2022-06-05 10:25:53 +0700

    project init

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 import CartProvider from './store/CartProvider';
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+  const handleShowCart = () => {
+    setCartIsShown(true)
+  }
+  const handleCloseCart = () => {
+    setCartIsShown(false)
+  }
   return (
     <CartProvider>
-     
+      {cartIsShown && <Cart onCloseCart={handleCloseCart}></Cart>}
+      <Header onShowCart={handleShowCart}></Header>
+      <main>
+        <Meals></Meals>
+      </main>
     </CartProvider>
   );
 }
